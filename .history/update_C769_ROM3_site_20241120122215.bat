c0dfbd2c22fd32b499136f372a59f2bf242c11af
Jim <ashejim@gmail.com>
2024-11-20 12:35:16 -0500

    fixed link. wired error with jb build --all

--- .history/update_C769_ROM3_site_20241120122215.bat
+++ .history/update_C769_ROM3_site_20241120122215.bat
@@ -1,7 +1,7 @@
 @echo off
 SET /P Message=Enter git C769-ROM3 commit comment:
 title C769-ROM3: compile html, save to repo folder, update GitHub repo and website
-cd "D:\OneDrive - Western Governors University\jupyter-books\C769-ROM3"
+cd D:\OneDrive - Western Governors University\jupyter-books\C769-ROM3
 FOR /d /r . %%d IN (.history) DO @IF EXIST "%%d" rd /s /q "%%d"
 jupyter-book build --all "D:\OneDrive - Western Governors University\jupyter-books\C769-ROM3"
 echo "Compiling local D:\OneDrive - Western Governors University\jupyter-books\C769-ROM3 ..."
